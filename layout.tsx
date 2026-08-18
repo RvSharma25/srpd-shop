32f41919af25c70e3b97d0c47e72b1900bbab6a1
RvSharma25 <159568004+RvSharma25@users.noreply.github.com>
2026-08-18 13:29:14 +0530

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,4 +0,0 @@
-import './globals.css';
-import { ReactNode } from 'react';
-export const metadata={title:'SRPD Shop',description:'Simple, dynamic campus shop'};
-export default function RootLayout({children}:{children:ReactNode}){return <html lang="en"><body>{children}</body></html>}
